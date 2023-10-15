162f908d5cad72ef6131b5a8b2a07ad5801f1db3
shundroid <shundroid@users.noreply.github.com>
2023-10-15 18:49:53 +0900

    updated for v0.1.0

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,13 +3,16 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
   logo: <span>flow</span>,
-  // project: {
-  //   link: 'https://github.com/shuding/nextra-docs-template',
-  // },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Created by flow-lab',
+  project: {
+    link: 'https://github.com/flow-org/flow',
   },
+  docsRepositoryBase: 'https://github.com/flow-org/flow-site/blob/main',
+  footer: {
+    text: 'Created by flow-org',
+  },
+  head: (<>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="128x128" />
+  </>)
 }
 
 export default config
